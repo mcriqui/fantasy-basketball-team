0910ee0dae4aaa49a717016dc67b7d34728b64b3
mcriqui <maggie.criqui@gmail.com>
2014-11-28 13:29:43 -0500

    Updated Admin

--- basketball/admin.py
+++ basketball/admin.py
@@ -3,5 +3,10 @@ from django.contrib import admin
 from basketball.models import Game, Player
 # Register your models here.
 
-admin.site.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('home_team', 'away_team','played_on_time', 'played_on_date', 'tv_channel')
+    list_filter = ('tv_channel', 'home_team', 'away_team')
+
+admin.site.register(Game, GameAdmin)
+
 admin.site.register(Player)
