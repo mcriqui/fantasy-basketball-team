36ebac5f4a8bc310b49681e3a844a702f00e1e57
mcriqui <maggie.criqui@gmail.com>
2015-01-03 16:16:19 -0500

    Updated views and templates

--- basketball/views.py
+++ basketball/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 import datetime
-from basketball.models import Game
+from basketball.models import Game, Player
+from django.db.models import Q
+
+
+# def todays_date(request):
+#     game_list = Game.objects.filter(played_on_date=datetime.date.today()).order_by('-played_on_time')
+#     context = {'game_list': game_list}
+#     return render(request, 'home.html', context)
 
 
 def todays_date(request):
-    game_list = Game.objects.filter(played_on_date=datetime.date.today()).order_by('-played_on_time')
+    players = Player.objects.all()
+    players_teams = [team.team for team in players]
+    game_list = Game.objects.filter(Q(home__in=players_teams) | Q(away__in=players_teams)).filter(played_on_date=datetime.date.today())
     context = {'game_list': game_list}
     return render(request, 'home.html', context)
