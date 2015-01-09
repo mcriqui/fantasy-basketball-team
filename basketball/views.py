9a35fef737d7b729bb161a5e6fc533ab2a94a9fc
mcriqui <maggie.criqui@gmail.com>
2015-01-09 14:39:21 -0500

    added query for players playing against wizards

--- basketball/views.py
+++ basketball/views.py
@@ -8,5 +8,7 @@ def todays_date(request):
     players = Player.objects.all()
     players_teams = [team.team for team in players]
     game_list = Game.objects.filter(Q(home__in=players_teams) | Q(away__in=players_teams)).filter(played_on_date=datetime.date.today())
-    context = {'game_list': game_list}
+    wizards_home = Game.objects.filter(home__city="Washington").filter(away__in=players_teams).filter(played_on_date__contains=datetime.date(2014, 12, 21))
+    wizards_away = Game.objects.filter(away__city="Washington").filter(home__in=players_teams).filter(played_on_date__contains=datetime.date(2014, 12, 25))
+    context = {'game_list': game_list, 'wizards_home': wizards_home, 'wizards_away': wizards_away}
     return render(request, 'home.html', context)
