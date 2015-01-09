dbe5c197ebf2df1017a7b50f81c86d5c4ded36a0
mcriqui <maggie.criqui@gmail.com>
2015-01-09 10:22:03 -0500

    updated design

--- basketball/views.py
+++ basketball/views.py
@@ -4,12 +4,6 @@ from basketball.models import Game, Player
 from django.db.models import Q
 
 
-# def todays_date(request):
-#     game_list = Game.objects.filter(played_on_date=datetime.date.today()).order_by('-played_on_time')
-#     context = {'game_list': game_list}
-#     return render(request, 'home.html', context)
-
-
 def todays_date(request):
     players = Player.objects.all()
     players_teams = [team.team for team in players]
